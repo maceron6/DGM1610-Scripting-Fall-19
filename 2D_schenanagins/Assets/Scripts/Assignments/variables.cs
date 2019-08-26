c54983717f54d730a2f3bd7d193a3129b59fbfa7
Cam the Mighty <maceron6@gmail.com>
2019-08-26 17:11:16 -0600

    added "variables"

---
+++ 2D_schenanagins/Assets/Scripts/Assignments/variables.cs
@@ -0,0 +1,21 @@
+﻿// PRAISE THE OMNISIAH
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour
+{
+    string name = "Cam the Mighty";
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(name);
+    }
+
+    
+}
+
+// PRAISE THE OMNISAIH
