de4fd18c7a7be456ebafd2eb5b012885dde4329d
Cam the Mighty <maceron6@gmail.com>
2019-09-04 17:06:47 -0600

    functions

---
+++ 2D_schenanagins/Assets/Scripts/Assignments/functions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class functions : MonoBehaviour
+{
+    // string firstName;
+    // string lastName;
+    // int age;
+
+    int num1;
+    int num2;
+    int total;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // firstName = "Billbo";
+        // lastName = "Baggins";
+        // age = 111;
+        // Customer(firstName, lastName, age);
+        CalcAdd(11, 23);
+    }
+    
+    // Update is called once per frame
+    // try not to use for games
+    int CalcAdd (int n1, int n2)
+    {
+        Debug.Log("#1 = " + n1 + "#2 = " + n2);
+        total = n1 + n2;
+        Debug.Log(total);
+        return total;
+        
+    }
+
+    // void Customer(string fName, string Name, int a)
+    // {
+    //     Debug.Log(fName + " " + lName + " " + a);
+    // }
+}
