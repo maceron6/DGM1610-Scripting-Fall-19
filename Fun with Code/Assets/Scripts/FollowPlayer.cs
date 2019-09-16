94b9e3e6147ebfb68c0b623e8af14927e1bcee7e
Cam the Mighty <maceron6@gmail.com>
2019-09-16 17:05:57 -0600

    added player and camera scripts

---
+++ Fun with Code/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 9, -31);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // make the camera follow the bus
+        transform.position = player.transform.position + offset;
+
+    }
+}
---
+++ Fun with Code/Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //it moves, whoopty doo :|
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
