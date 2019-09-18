810620c43a4779a1b6ea3745e50d0f6d342375bb
Cam the Mighty <maceron6@gmail.com>
2019-09-18 16:53:58 -0600

    ho crap i hope this works

--- Fun with Code/Assets/Scripts/PlayerControll.cs
+++ Fun with Code/Assets/Scripts/PlayerControll.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerControll : MonoBehaviour
 {
 
-    public float speed = 5.0f;
+    //private float speed = 50.0f;
+    private float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,12 @@ public class PlayerControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //it moves, whoopty doo :|
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardInput *25);
+        //turn right or left
+        //transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.up, horizontalInput);
     }
 }
