4ed7ba5d413e9548555bae603792e73bd80c8e09
Cam the Mighty <maceron6@gmail.com>
2019-09-23 16:35:12 -0600

    I fixed the plain game

--- Chalenge 1 Plane/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Chalenge 1 Plane/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3 (0, 9, -33);
+    private Vector3 offset = new Vector3 (27, 3, 10);
 
     // Start is called before the first frame update
     void Start()
