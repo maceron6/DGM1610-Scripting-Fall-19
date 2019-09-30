f170ad55b1231e5fc9014ea6a609e98b68ec633d
Cam the Mighty <maceron6@gmail.com>
2019-09-30 17:08:08 -0600

    added player controlls to agressive feeding

---
+++ agressive feeding/Assets/Course Library/Scripts/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float horizontalImput;
+    private float speed = 20f;
+    public float xrange = 22.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xrange)
+        {
+            transform.position = new Vector3(-xrange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xrange)
+        {
+            transform.position = new Vector3(xrange, transform.position.y, transform.position.z);
+        }
+
+
+        horizontalImput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalImput * Time.deltaTime * speed);
+    }
+}
