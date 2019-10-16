b578545324c44d2d89f97b0590883e5c46f7af4b
Cam the Mighty <maceron6@gmail.com>
2019-10-16 16:03:29 -0600

    added scripts

--- Thunder Hawk/Assets/Scripts/KillBoundery.cs
+++ Thunder Hawk/Assets/Scripts/KillBoundery.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class KillBoundery : MonoBehaviour
 {
+    private float topBound = -5.0f;
+    private float bottomBound = -15.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class KillBoundery : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.z < topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z > bottomBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Thunder Hawk/Assets/Scripts/PlayerControll.cs
+++ Thunder Hawk/Assets/Scripts/PlayerControll.cs
@@ -8,6 +8,7 @@ public class PlayerControll : MonoBehaviour
     public float horzInput;
     public float vertInput;
     public float speed = 10f;
+    public GameObject projectilePref;
     
     // Start is called before the first frame update
     void Start()
@@ -18,16 +19,17 @@ public class PlayerControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -xrange)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = new Vector3(-xrange, transform.position.y, transform.position.z);
+            Instantiate(projectilePref, transform.position, projectilePref.transform.rotation);
         }
-
-        if (transform.position.x > xrange)
-        {
-            transform.position = new Vector3(xrange, transform.position.y, transform.position.z);
-        }
-
         
+        horzInput = Input.GetAxis("Horizontal");
+
+        vertInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horzInput * Time.deltaTime * speed);
+
+        transform.Translate(Vector3.forward * vertInput * Time.deltaTime * speed);
     }
 }
