fa301dd98f766a947da90501cbc48d7dd6423a10
Cam the Mighty <maceron6@gmail.com>
2019-10-16 17:08:13 -0600

    created project 3

---
+++ Project.3/Assets/Course Library/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float jumpForce;
+    public float gravMod;
+    public bool onGround = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+       // playerRB.AddForce(Vector3.up * 1000);
+       Physics.gravity *= gravMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            onGround = false;
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+    }
+}
---
+++ Project.3/Assets/Course Library/Scripts/Spawnner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnner : MonoBehaviour
+{
+    public GameObject pipePre;
+    private Vector3 spawnPos = new Vector3 (25, 0, -4);
+    public float spawnDelay = 2f;
+    public float startDelay = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnPipe", startDelay, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void SpawnPipe()
+    {
+        Instantiate(pipePre, spawnPos, pipePre.transform.rotation);
+    }
+}
